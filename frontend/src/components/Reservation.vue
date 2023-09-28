e621d368ee22f671ad4fbb1c3cf155f63bd58a10
Maxim Safronov <macksafronov@yandex.ru>
2023-09-28 12:28:04 +0300

    Front + Backend  (28.09.23)

---
+++ frontend/src/components/Reservation.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const reservations = ref([])
+
+const GetReservation = async () => {
+  await fetch('http://localhost:6060/orders').then((response) => {
+    reservations.value = response.data
+  })
+}
+GetReservation()
+export default {
+  name: "Reservation"
+}
+</script>
+
+<template>
+  <ul>
+    <li v-for="reservation in reservations" :key="reservation.id">
+      <p>
+        {{reservation.name}}
+      </p>
+      <p>
+        {{reservation.price}}
+      </p>
+    </li>
+  </ul>
+</template>
