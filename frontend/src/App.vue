3b69e863a3dfd41476790b14f6c36e6d21ca2ea8
Maxim Safronov <macksafronov@yandex.ru>
2023-09-30 13:13:07 +0300

    Front + Backend  (28.09.23) Created 2 entity Errors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <TimeMoscow/>
   <ReservationItems/>
 </template>
 
 <script>
+import TimeMoscow from './components/TimeMoscow.vue'
 import ReservationItems from './components/Reservation.vue'
 
 export default {
   name: 'App',
   components: {
+    TimeMoscow,
     ReservationItems
   }
 }
+
 </script>
 
 <style>
--- frontend/src/components/Reservation.vue
+++ frontend/src/components/Reservation.vue
@@ -19,22 +19,26 @@ const GetProduct = async () => {
 
 GetReservation()
 GetProduct()
+
 </script>
 
 <template>
-
-  <div v-if="reservations">
+  <div>
     <h5>Заказы</h5>
     <ul>
       <li v-for="reservation in reservations" :key="reservation.id">
-        {{ reservation.id}} {{ reservation.name }} {{ reservation.address }}
+        {{ reservation.id }}
+        {{ reservation.name }} {{ reservation.address }}
         {{ reservation.amount }} {{ reservation.date_creation.toString()}}
-      </li>
-      <li v-for="product in products" :key="product.id">
-        {{ product.serial }} {{ product.name }} {{ product.quantity }}
+        <ul>
+          <li v-for="(product, index) in getProductsByReservationId(reservation.id)" :key="index">
+              {{ product.id }} {{ product.serial }} {{ product.name }} {{ product.quantity }}
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
+
   <div v-if="!reservations" >
     Error
   </div>
@@ -42,6 +46,17 @@ GetProduct()
 
 <script>
 export default {
-  name: "ReservationItems"
+  name: "ReservationItems",
+  data() {
+    return {
+      reservations: [],
+      products: []
+    }
+  },
+  computed: {
+    getProductsByReservationId:function(reservationId) {
+      return this.products.filter(product => product.orders_id === reservationId);
+    }
+  }
 }
 </script>
